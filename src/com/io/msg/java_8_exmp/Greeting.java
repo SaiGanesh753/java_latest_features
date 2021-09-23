ba247cbabaf09b198c7f1ce65f4dde29cbeb3de9
saiganesh753 <saiganeshmutyala@gmail.com>
2021-09-23 22:47:08 +0530

    sample test file

---
+++ src/com/io/msg/java_8_exmp/Greeting.java
@@ -0,0 +1,14 @@
+package java_8_exmp;
+
+public class Greeting {
+	
+	public void greeting() {
+		System.out.println("hi sai from greeting java");
+	}
+
+	public static void main(String[] args) {
+		Greeting greet = new Greeting();
+		greet.greeting();
+	}
+
+}
