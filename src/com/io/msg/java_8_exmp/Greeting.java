ebdfb64007ab8e684023d130eba4fc04eeb6caba
saiganesh753 <saiganeshmutyala@gmail.com>
2021-09-24 11:10:07 +0530

    test 1

--- src/com/io/msg/java_8_exmp/Greeting.java
+++ src/com/io/msg/java_8_exmp/Greeting.java
@@ -3,7 +3,7 @@ package java_8_exmp;
 public class Greeting {
 	
 	public void greeting() {
-		System.out.println("hi sai from greeting java");
+		System.out.println("hi sai from greeting java1");
 	}
 
 	public static void main(String[] args) {
